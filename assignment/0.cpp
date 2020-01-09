f550baa573b84d6d2f556d21a9639e805463b3d5
Philip Kobernik <ptk921@gmail.com>
2020-01-08 19:06:05 -0800

    assignment 0... using tokens and for loop

---
+++ assignment/0.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+int main() {
+  while (true) {
+    printf("Type a sentence (then hit return): ");
+
+    string line;
+    string word;
+    // vector of strings
+    vector <string> tokens;
+    getline(cin, line);
+    stringstream stream_line(line);
+
+    while ( getline(stream_line, word, ' ')) {
+      tokens.push_back(word);
+    }
+
+    if (!cin.good()) {
+      printf("Done\n");
+      return 0;
+    }
+
+    for(int i = 0; i < tokens.size(); i++) {
+      for(int j = tokens[i].size()-1; j > -1; j--) {
+        cout << tokens[i][j];
+      }
+      cout << ' ';
+    }
+    cout << "\n\ngood jorb!\n\n";
+
+  }
+}
