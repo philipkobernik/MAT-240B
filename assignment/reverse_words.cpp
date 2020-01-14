cf133d0f18736b7305d442148e12c5140ecb43fb
Philip Kobernik <ptk921@gmail.com>
2020-01-13 23:49:53 -0800

    use reverse iterator

--- assignment/reverse_words.cpp
+++ assignment/reverse_words.cpp
@@ -25,9 +25,14 @@ int main() {
     }
 
     for(int i = 0; i < tokens.size(); i++) {
-      for(int j = tokens[i].size()-1; j > -1; j--) {
-        cout << tokens[i][j];
+      // reverse iterator
+      for (std::string::reverse_iterator rit=tokens[i].rbegin(); rit!=tokens[i].rend(); ++rit) {
+        cout << *rit;
       }
+      // my for loop
+      //for(int j = tokens[i].size()-1; j > -1; j--) {
+        //cout << tokens[i][j];
+      //}
       cout << ' ';
     }
     cout << "\n\ngood jorb!\n\n";
