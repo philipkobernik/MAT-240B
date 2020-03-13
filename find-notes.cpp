8dbb9ea92cfd3c41a1ae846c47f817c79a52ee91
Philip Kobernik <ptk921@gmail.com>
2020-03-13 03:03:28 -0700

    find-notes writes out csv files for each semitone

--- find-notes.cpp
+++ find-notes.cpp
@@ -30,8 +30,15 @@ struct Note {
 	}
 };
 
+struct NoteFile {
+	int midiPitch;
+	ofstream out;
+	ofstream outNormNoIndex;
+};
+
 int main() {
 	vector<Note> notes;
+	vector<NoteFile> files;
 	vector<float> maxx;
 	vector<float> minn;
 	ofstream outFile, outFileNormNoIndex;
@@ -134,30 +141,75 @@ int main() {
 			  << notes[i].lengthInFrames << "," << notes[i].rms
 			  << "," << notes[i].tone << "," << notes[i].onset
 			  << "," << notes[i].peakiness << endl;
-		cout << outStream.str();
+		// cout << outStream.str();
 		outFile << outStream.str();
 	}
 
 	// end pads.csv
 	outFile.close();
 
+	vector<int> noteCounts(40, 0);
+
 	// write pads.norm.no.index.csv
 	for (int i = 0; i < notes.size(); i++) {
-      Note n = notes[i];
-			outFileNormNoIndex
-				//<< (n.rms - minn[0]) / (maxx[0] - minn[0])
-				//<< ","
-				//<< (n.tone - minn[1]) / (maxx[1] - minn[1])
-				//<< ","
-				//<< (n.onset - minn[3]) / (maxx[3] - minn[3])
-				//<< ","
-				//<< (n.peakiness - minn[2]) / (maxx[2] - minn[2])
-				//<< ","
-				<< (n.midiPitch - minn[4]) / (maxx[4] - minn[4])
-				<< endl;
+		Note n = notes[i];
+		outFileNormNoIndex
+		    //<< (n.rms - minn[0]) / (maxx[0] - minn[0])
+		    //<< ","
+		    //<< (n.tone - minn[1]) / (maxx[1] - minn[1])
+		    //<< ","
+		    //<< (n.onset - minn[3]) / (maxx[3] - minn[3])
+		    //<< ","
+		    //<< (n.peakiness - minn[2]) / (maxx[2] - minn[2])
+		    //<< ","
+		    << (n.midiPitch - minn[4]) / (maxx[4] - minn[4]) << endl;
 
-			//int p = n.midiPitch;
-			//float ramp = n.midiPitch - p;
+		int p = n.midiPitch;
+		float ramp = n.midiPitch - p;
+
+		for (int j = 24; j < noteCounts.size(); j++) {
+			if ((n.midiPitch > float(j) - 0.33f) &&
+			    (n.midiPitch < float(j) + 0.33f)) {
+				noteCounts[j] = noteCounts[j] + 1;
+				if (files.size() < j - 23) {
+          char outName[30];
+          char outNameNoIndex[30];
+					sprintf(outName, "note-%d.csv", j);
+					sprintf(outNameNoIndex, "note-%d.no.index.csv", j);
+					files.emplace_back();
+					files.back().out.open(outName);
+					files.back().outNormNoIndex.open(outNameNoIndex);
+					files.back().midiPitch = j;
+					// files should be primed for writing
+				}
+				files.back().out << notes[i].lineIndex << ","
+					     << notes[i].sampleLocation << ","
+					     << notes[i].midiPitch << ","
+					     << notes[i].lengthInFrames << ","
+					     << notes[i].rms << ","
+					     << notes[i].tone << ","
+					     << notes[i].onset << ","
+					     << notes[i].peakiness << endl;
+
+				files.back().outNormNoIndex 
+				<< (n.rms - minn[0]) / (maxx[0] - minn[0])
+				<< ","
+				<< (n.tone - minn[1]) / (maxx[1] - minn[1])
+				<< ","
+				<< (n.onset - minn[3]) / (maxx[3] - minn[3])
+				<< ","
+				<< (n.peakiness - minn[2]) / (maxx[2] - minn[2])
+		    << endl;
+			}
+		}
+	}
+	for (int j = 24; j < noteCounts.size(); j++) {
+		std::cout << "noteCounts: " << j << ": " << noteCounts[j]
+			  << std::endl;
+	}
+	for (int j = 0; j < files.size(); j++) {
+    files[j].out.close();
+    files[j].outNormNoIndex.close();
 	}
 
 	outFileNormNoIndex.close();
